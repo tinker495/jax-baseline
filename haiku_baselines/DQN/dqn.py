bd65eeca1c68e6bb100e025affa2067d55f8f3e0
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-11-12 18:57:16 +0900

    asdfasdfasdfasdf

--- haiku_baselines/DQN/dqn.py
+++ haiku_baselines/DQN/dqn.py
@@ -109,9 +109,9 @@ class DQN(Q_Network_Family):
 
     def _train_step(self, params, target_params, opt_state, steps, obses, actions, rewards, nxtobses, dones, weights=1, indexes=None):
         obses = convert_jax(obses); nxtobses = convert_jax(nxtobses); actions = actions.astype(jnp.int32)
-        rewards = jnp.expand_dims(rewards, axis=1);  not_dones = jnp.expand_dims(1 - dones,axis=1)
+        rewards = rewards;  not_dones = 1 - dones
         targets = self._target(params, target_params,obses, actions, rewards, nxtobses, not_dones)
-        print(rewards.shape,targets.shape)
+        print(rewards.shape,not_dones.shape,targets.shape)
         loss, grad = jax.value_and_grad(self._loss)(params, obses, actions, targets, weights)
         updates, opt_state = self.optimizer.update(grad, opt_state, params)
         online_params = optax.apply_updates(params, updates)
