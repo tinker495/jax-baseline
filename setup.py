d04d7a2876a7c11b50659bd7180760cb7a3a1a47
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-11-18 20:18:05 +0900

    asdfasdf

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
         'gym',
         'box2d',
         'box2d-py',
-        'git://github.com/kenjyoung/MinAtar.git#egg=MinAtar',
+        '-e git://github.com/kenjyoung/MinAtar.git#egg=MinAtar',
         'dm-haiku',
         'optax',
         'numpy',
