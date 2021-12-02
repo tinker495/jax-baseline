c27131a3a4203d6f0d3a0da9054fb23bf20cd4f0
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-12-02 10:43:14 +0900

    asdfasdfasdfasdf

--- haiku_baselines/common/atari_wrappers.py
+++ haiku_baselines/common/atari_wrappers.py
@@ -265,7 +265,7 @@ def make_atari(env_id, max_episode_steps=None):
     env = gym.make(env_id)
     #assert 'NoFrameskip' in env.spec.id
     if 'NoFrameskip' in env.spec.id:
-        #env = NoopResetEnv(env, noop_max=30)
+        env = NoopResetEnv(env, noop_max=30)
         env = MaxAndSkipEnv(env, skip=4)
     if max_episode_steps is not None:
         env = TimeLimit(env, max_episode_steps=max_episode_steps)
