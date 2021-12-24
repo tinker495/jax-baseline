d866c16914c0c0ebc0cfcd4a050e2593420ccd6d
TinkTheBoush <wjdrbtjr495@gmail.com>
2021-12-24 10:35:05 +0900

    asdfasdfasdfasdf

--- haiku_baselines/QRDQN/qrdqn.py
+++ haiku_baselines/QRDQN/qrdqn.py
@@ -109,7 +109,7 @@ class QRDQN(Q_Network_Family):
         return params, target_params, opt_state, loss, jnp.mean(targets), new_priorities
     
     def _loss(self, params, obses, actions, targets, weights, key):
-        theta_loss_tile = jnp.take_along_axis(self.get_q(params, obses, tau, key), actions, axis=1) # batch x 1 x (support x dual_axis)
+        theta_loss_tile = jnp.take_along_axis(self.get_q(params, obses, key), actions, axis=1) # batch x 1 x (support x dual_axis)
         logit_valid_tile = jnp.expand_dims(targets,axis=2)                                          # batch x (support x dual_axis) x 1
         error = logit_valid_tile - theta_loss_tile                                              # batch x (support x dual_axis) x (support x dual_axis)
         huber = ((jnp.abs(error) <= self.delta).astype(jnp.float32) *
