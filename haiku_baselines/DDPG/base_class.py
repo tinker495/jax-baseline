2299f7ff18e3f3c561b376b4388055dc36cca00b
TinkTheBoush <wjdrbtjr495@gmail.com>
2022-01-11 17:20:02 +0900

    asdfasdfasdf

--- haiku_baselines/DDPG/base_class.py
+++ haiku_baselines/DDPG/base_class.py
@@ -16,7 +16,7 @@ from mlagents_envs.environment import UnityEnvironment, ActionTuple
 
 class Deteministic_Policy_Gradient_Family(object):
     def __init__(self, env, gamma=0.99, learning_rate=5e-5, buffer_size=50000, train_freq=1, gradient_steps=1, batch_size=32,
-                 n_step = 1, learning_starts=1000, target_network_update_tau=2e-4, prioritized_replay=False,
+                 n_step = 1, learning_starts=1000, target_network_update_tau=5e-4, prioritized_replay=False,
                  prioritized_replay_alpha=0.6, prioritized_replay_beta0=0.4, prioritized_replay_eps=1e-6,
                  log_interval=200, tensorboard_log=None, _init_setup_model=True, policy_kwargs=None, 
                  full_tensorboard_log=False, seed=None, optimizer = 'adamw'):
