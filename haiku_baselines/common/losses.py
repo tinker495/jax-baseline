329b516df10c270e25bda4a0e5293a4f3c81c31d
TinkTheBoush <wjdrbtjr495@gmail.com>
2022-01-12 18:39:58 +0900

    asdfasdfasdf

--- haiku_baselines/common/losses.py
+++ haiku_baselines/common/losses.py
@@ -7,10 +7,10 @@ import numpy as np
 
 def HuberLosses(q_tile, target_tile,quantile,delta):
     error = target_tile - q_tile
-    #error = q_tile - target_tile
+    error_neg = jax.lax.stop_gradient((error < 0.).astype(jnp.float32))
+    weight = jnp.abs(quantile - error_neg)
     huber = ((jnp.abs(error) <= delta).astype(jnp.float32) *
             0.5 * error ** 2 +
             (jnp.abs(error) > delta).astype(jnp.float32) *
             delta * (jnp.abs(error) - 0.5 * delta))
-    mul = jax.lax.stop_gradient(jnp.abs(quantile - (error < 0).astype(jnp.float32)))
-    return jnp.sum(jnp.mean(mul*huber,axis=1),axis=1)
+    return jnp.sum(jnp.mean(weight*huber,axis=1),axis=1)
