c2b3001c28302c052740ed6a20f0ff6fbf056954
TinkTheBoush <wjdrbtjr495@gmail.com>
2022-01-12 18:02:56 +0900

    asdfasdfasdf

--- haiku_baselines/SAC/sac.py
+++ haiku_baselines/SAC/sac.py
@@ -79,7 +79,7 @@ class SAC(Deteministic_Policy_Gradient_Family):
             jnp.square((x_t - mu) / (std + 1e-6))
             + 2 * log_std
             + jnp.log(2 * np.pi)
-            ) - jnp.log(1 - jnp.square(pi) + 1e-6),axis=1)
+            ) - jnp.log(1 - jnp.square(pi) + 1e-6),axis=1,keepdims=True)
         return pi, log_prob, mu, log_std, std
         
     def _get_actions(self, params, obses, key = None) -> jnp.ndarray:
