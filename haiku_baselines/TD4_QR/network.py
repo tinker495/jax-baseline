ea4558cbd5ce35b4a8ce6debc7071f81b19e3d4f
TinkTheBoush <wjdrbtjr495@gmail.com>
2022-01-12 13:39:27 +0900

    asdfasdf

---
+++ haiku_baselines/TD4_QR/network.py
@@ -0,0 +1,53 @@
+import numpy as np
+import haiku as hk
+import jax
+import jax.numpy as jnp
+
+
+class Actor(hk.Module):
+    def __init__(self,action_size,node=256,hidden_n=2):
+        super(Actor, self).__init__()
+        self.action_size = action_size
+        self.node = node
+        self.hidden_n = hidden_n
+        self.layer = hk.Linear
+        
+    def __call__(self,feature: jnp.ndarray) -> jnp.ndarray:
+            action = hk.Sequential(
+                [
+                    self.layer(self.node) if i%2 == 0 else jax.nn.relu for i in range(2*self.hidden_n)
+                ] + 
+                [
+                    self.layer(self.action_size[0]),
+                    jax.nn.tanh
+                ]
+                )(feature)
+            return action
+        
+class Critic(hk.Module):
+    def __init__(self,node=256,hidden_n=2,support_n=200):
+        super(Critic, self).__init__()
+        self.node = node
+        self.hidden_n = hidden_n
+        self.support_n = support_n
+        self.layer = hk.Linear
+        
+    def __call__(self,feature: jnp.ndarray,actions: jnp.ndarray) -> jnp.ndarray:
+        concat = jnp.concatenate([feature,actions],axis=1)
+        q1_net = hk.Sequential(
+            [
+                self.layer(self.node) if i%2 == 0 else jax.nn.relu for i in range(2*self.hidden_n)
+            ] + 
+            [
+                self.layer(self.support_n)
+            ]
+            )(concat)
+        q2_net = hk.Sequential(
+            [
+                self.layer(self.node) if i%2 == 0 else jax.nn.relu for i in range(2*self.hidden_n)
+            ] + 
+            [
+                self.layer(self.support_n)
+            ]
+            )(concat)
+        return q1_net,q2_net
