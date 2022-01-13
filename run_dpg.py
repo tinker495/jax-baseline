5e4fadcb40fc09e68a6d48e1e5bc9e0ea957fbcf
TinkTheBoush <wjdrbtjr495@gmail.com>
2022-01-13 14:02:47 +0900

    asdfasdfasdf

--- run_dpg.py
+++ run_dpg.py
@@ -6,6 +6,7 @@ from haiku_baselines.DDPG.ddpg import DDPG
 from haiku_baselines.TD3.td3 import TD3
 from haiku_baselines.TD4_QR.td4_qr import TD4_QR
 from haiku_baselines.SAC.sac import SAC
+from haiku_baselines.TQC.tqc import TQC
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -74,6 +75,12 @@ if __name__ == "__main__":
         agent = SAC(env, gamma=args.gamma, batch_size = args.batch, buffer_size= int(args.buffer_size), target_network_update_tau = args.target_update_tau,
                     prioritized_replay = args.per, n_step = args.n_step, train_freq=args.train_freq, ent_coef = args.ent_coef,
                     tensorboard_log=args.logdir + env_type + "/" +env_name, policy_kwargs=policy_kwargs, optimizer=args.optimizer)
+    if args.algo == "TQC":
+        agent = TQC(env, gamma=args.gamma, batch_size = args.batch, buffer_size= int(args.buffer_size), target_network_update_tau = args.target_update_tau,
+                    prioritized_replay = args.per, n_step = args.n_step, train_freq=args.train_freq, ent_coef = args.ent_coef, n_support = args.n_support,
+                    tensorboard_log=args.logdir + env_type + "/" +env_name, policy_kwargs=policy_kwargs, optimizer=args.optimizer)
+        
+
 
 
     agent.learn(int(args.steps))
