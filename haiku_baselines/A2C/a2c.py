f289bb7b3a0f63a0d5ddb28960a6ab09deb34edc
TinkTheBoush <wjdrbtjr495@gmail.com>
2022-01-14 18:23:07 +0900

    asdfasdfasdf

--- haiku_baselines/A2C/a2c.py
+++ haiku_baselines/A2C/a2c.py
@@ -8,14 +8,14 @@ from haiku_baselines.A2C.base_class import Actor_Critic_Policy_Gradient_Family
 from haiku_baselines.A2C.network import Actor, Critic
 from haiku_baselines.common.schedules import LinearSchedule
 from haiku_baselines.common.Module import PreProcess
-from haiku_baselines.common.utils import soft_update, convert_jax
+from haiku_baselines.common.utils import convert_jax, get_gaes
 
 class A2C(Actor_Critic_Policy_Gradient_Family):
-    def __init__(self, env, gamma=0.99, learning_rate=5e-5, epoch_size=1000, gradient_steps=1, batch_size=32,
+    def __init__(self, env, gamma=0.99, learning_rate=5e-5, gradient_steps=1, batch_size=32,
                  log_interval=200, tensorboard_log=None, _init_setup_model=True, policy_kwargs=None, 
                  full_tensorboard_log=False, seed=None, optimizer = 'adamw'):
         
-        super(A2C, self).__init__(env, gamma, learning_rate, epoch_size, gradient_steps, batch_size,
+        super(A2C, self).__init__(env, gamma, learning_rate, gradient_steps, batch_size,
                  log_interval, tensorboard_log, _init_setup_model, policy_kwargs, 
                  full_tensorboard_log, seed, optimizer)
         
@@ -28,7 +28,7 @@ class A2C(Actor_Critic_Policy_Gradient_Family):
             cnn_mode = self.policy_kwargs['cnn_mode']
             del self.policy_kwargs['cnn_mode']
         self.preproc = hk.transform(lambda x: PreProcess(self.observation_space, cnn_mode=cnn_mode)(x))
-        self.actor = hk.transform(lambda x: Actor(self.action_size,self.action_type,**self.policy_kwargs)(x))
+        self.actor = hk.transform(lambda x: Actor(self.action_size,**self.policy_kwargs)(x))
         self.critic = hk.transform(lambda x,a: Critic(**self.policy_kwargs)(x,a))
         pre_param = self.preproc.init(next(self.key_seq),
                             [np.zeros((1,*o),dtype=np.float32) for o in self.observation_space])
@@ -36,7 +36,6 @@ class A2C(Actor_Critic_Policy_Gradient_Family):
         actor_param = self.actor.init(next(self.key_seq), feature)
         critic_param = self.critic.init(next(self.key_seq), feature, np.zeros((1,self.action_size[0])))
         self.params = hk.data_structures.merge(pre_param, actor_param, critic_param)
-        self.target_params = self.params
         
         self.opt_state = self.optimizer.init(self.params)
         
@@ -45,40 +44,42 @@ class A2C(Actor_Critic_Policy_Gradient_Family):
         print(jax.tree_map(lambda x: x.shape, actor_param))
         print(jax.tree_map(lambda x: x.shape, critic_param))
         print("-------------------------------------------------")
-
+        
         self._get_actions = jax.jit(self._get_actions)
         self._train_step = jax.jit(self._train_step)
         
-    def _get_actions(self, params, obses, key = None) -> jnp.ndarray:
-        return self.actor.apply(params, key, self.preproc.apply(params, key, convert_jax(obses))) #
+    def _get_actions_discrete(self, params, obses, key = None) -> jnp.ndarray:
+        prob = jax.nn.softmax(self.actor.apply(params, None, self.preproc.apply(params, None, convert_jax(obses))),axis=1)
+        return prob
+    
+    def _get_actions_continuous(self, params, obses, key = None) -> jnp.ndarray:
+        mu,std = self.actor.apply(params, None, self.preproc.apply(params, None, convert_jax(obses)))
+        return mu, std
     
     def discription(self):
         return "score : {:.3f}, epsilon : {:.3f}, loss : {:.3f} |".format(
                                     np.mean(self.scoreque), self.epsilon, np.mean(self.lossque)
                                     )
     
-    def actions(self,obs,steps):
-        self.epsilon = self.exploration.value(steps)
-        actions = np.clip(np.asarray(self._get_actions(self.params,obs, None)) + self.noise()*self.epsilon,-1,1)
-        return actions
+    def action_discrete(self,obs,steps):
+        prob = self._get_actions(self.params, obs)
+        return np.stack([np.random.choice(self.action_size[0],p=p) for p in prob],axis=0)
+    
+    def action_continuous(self,obs,steps):
+        mu, std = self._get_actions(self.params, obs)
+        return mu
     
     def test_action(self, state):
         return np.clip(np.asarray(self._get_actions(self.params,state, None)) + self.noise()*self.exploration_final_eps,-1,1)
     
     def train_step(self, steps, gradient_steps):
         # Sample a batch from the replay buffer
+        data = self.buffer.get_buffer()
         for _ in range(gradient_steps):
-            if self.prioritized_replay:
-                data = self.replay_buffer.sample(self.batch_size,self.prioritized_replay_beta0)
-            else:
-                data = self.replay_buffer.sample(self.batch_size)
             
-            self.params, self.target_params, self.opt_state, loss, t_mean, new_priorities = \
+            self.params, self.target_params, self.opt_state, loss, t_mean = \
                 self._train_step(self.params, self.target_params, self.opt_state, None,
                                  **data)
-            
-            if self.prioritized_replay:
-                self.replay_buffer.update_priorities(data['indexes'], new_priorities)
             
         if self.summary and steps % self.log_interval == 0:
             self.summary.add_scalar("loss/qloss", loss, steps)
@@ -86,21 +87,18 @@ class A2C(Actor_Critic_Policy_Gradient_Family):
             
         return loss
 
-    def _train_step(self, params, target_params, opt_state, key, 
+    def _train_step_discrete(self, params, opt_state, key, 
+                    obses, actions, rewards, nxtobses, dones, weights=1, indexes=None):
+        obses = [convert_jax(o) for o in obses]; nxtobses = [convert_jax(n) for n in nxtobses]; not_dones = [1.0 - d for d in dones]
+        value = [self.critic.apply(params, key, self.preproc.apply(params, None, o)) for o in obses]
+        next_value = [self.critic.apply(params, key, self.preproc.apply(params, None, n)) for n in nxtobses]
+        adv, targets = zip(*[get_gaes(r, d, v, nv, self.gamma, self.lamda, self.gae_normalize) for r, d, v, nv in zip(rewards, dones, value, next_value)])
+        return 
+    
+    def _train_step_continuous(self, params, target_params, opt_state, key, 
                     obses, actions, rewards, nxtobses, dones, weights=1, indexes=None):
         obses = convert_jax(obses); nxtobses = convert_jax(nxtobses); not_dones = 1.0 - dones
-        targets = self._target(target_params, rewards, nxtobses, not_dones, key)
-        (critic_loss,abs_error), critic_grad = jax.value_and_grad(self._critic_loss,has_aux = True)(params, obses, actions, targets, weights, key)
-        updates, opt_state = self.optimizer.update(critic_grad, opt_state, params=params)
-        params = optax.apply_updates(params, updates)
-        actor_loss, actor_grad = jax.value_and_grad(self._actor_loss)(params, obses, key)
-        updates, opt_state = self.optimizer.update(actor_grad, opt_state, params=params)
-        params = optax.apply_updates(params, updates)
-        target_params = soft_update(params, target_params, self.target_network_update_tau)
-        new_priorities = None
-        if self.prioritized_replay:
-            new_priorities = abs_error + self.prioritized_replay_eps
-        return params, target_params, opt_state, critic_loss, actor_loss, new_priorities
+        return params, target_params, opt_state, critic_loss, actor_loss
     
     def _critic_loss(self, params, obses, actions, targets, weights, key):
         feature = self.preproc.apply(params, key, obses)
