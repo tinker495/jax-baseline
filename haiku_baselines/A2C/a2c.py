ec8d61b18892e8ac8db926af3c92599079ccf735
TinkTheBoush <wjdrbtjr495@gmail.com>
2022-01-14 21:35:33 +0900

    asdfasdfasdfasdfasdfasdfasdfasdf

--- haiku_baselines/A2C/a2c.py
+++ haiku_baselines/A2C/a2c.py
@@ -91,7 +91,7 @@ class A2C(Actor_Critic_Policy_Gradient_Family):
         obses = [convert_jax(o) for o in obses]; nxtobses = [convert_jax(n) for n in nxtobses]
         value = [self.critic.apply(params, key, self.preproc.apply(params, None, o)) for o in obses]
         next_value = [self.critic.apply(params, key, self.preproc.apply(params, None, n)) for n in nxtobses]
-        obses_hstack = [jnp.hstack(o) for o in zip(*obses)]
+        obses_hstack = [jnp.hstack(zo) for zo in list(zip(*obses))]
         adv, targets = zip(*[get_gaes(r, d, v, nv, self.gamma, self.lamda, self.gae_normalize) for r, d, v, nv in zip(rewards, dones, value, next_value)])
         (total_loss, (critic_loss, actor_loss)), grad = jax.value_and_grad(self._loss,has_aux = True)(params, 
                                                         obses_hstack, jnp.hstack(actions), jnp.hstack(targets), jnp.hstack(adv), ent_coef, key)
