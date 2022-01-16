26229e697d8a1c30692afd66e8c3dca13372cc41
TinkTheBoush <wjdrbtjr495@gmail.com>
2022-01-16 11:28:04 +0900

    asdfasdfasdfasdf

--- haiku_baselines/A2C/a2c.py
+++ haiku_baselines/A2C/a2c.py
@@ -91,8 +91,10 @@ class A2C(Actor_Critic_Policy_Gradient_Family):
         value = [self.critic.apply(params, key, self.preproc.apply(params, key, o)) for o in obses]
         next_value = [self.critic.apply(params, key, self.preproc.apply(params, key, n)) for n in nxtobses]
         adv, targets = zip(*[get_gaes(r, d, t, v, nv, self.gamma, self.lamda, self.gae_normalize) for r, d, t, v, nv in zip(rewards, dones, terminals, value, next_value)])
+        unzip_obses = [jnp.hstack(zo) for zo in list(zip(*obses))]
+        print(unzip_obses.shape)
         (total_loss, (critic_loss, actor_loss)), grad = jax.value_and_grad(self._loss,has_aux = True)(params, 
-                                                        [jnp.hstack(zo) for zo in list(zip(*obses))], jnp.hstack(actions), jnp.hstack(targets), jnp.hstack(adv), ent_coef, key)
+                                                        unzip_obses, jnp.hstack(actions), jnp.hstack(targets), jnp.hstack(adv), ent_coef, key)
         updates, opt_state = self.optimizer.update(grad, opt_state, params=params)
         params = optax.apply_updates(params, updates)
         return params, opt_state, critic_loss, actor_loss
