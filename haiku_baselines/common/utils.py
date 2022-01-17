721d1eb080d4bcc8cfa52dfb1c22345ef6404095
TinkTheBoush <wjdrbtjr495@gmail.com>
2022-01-17 14:26:28 +0900

    asdfasdfasdf

--- haiku_baselines/common/utils.py
+++ haiku_baselines/common/utils.py
@@ -45,8 +45,8 @@ def discount_with_terminal(rewards, dones, terminals, next_values, gamma):
     ret = reward + gamma * (ret * (1. - term) + nextval * (1. - done) * term)
     return ret, ret
   ret = rewards[-1] + gamma * next_values[-1] * (1. - dones[-1])
-  _, discounted = jax.lax.scan(f, ret, (rewards[-2::-1], dones[-2::-1], terminals[-2::-1], next_values[-2::-1]))
-  return jnp.append(discounted[::-1], ret)
+  _, discounted = jax.lax.scan(f, ret, (rewards[:-1], dones[:-1], terminals[:-1], next_values[:-1]),reverse=True)
+  return jnp.append(jnp.flip(discounted), ret)
 
 '''
 def discount_with_terminal(rewards, dones, terminals, next_values, gamma):
