05d12806b9591a7ee153b14f64d0d63f6f5326f9
TinkTheBoush <wjdrbtjr495@gmail.com>
2022-01-17 15:26:18 +0900

    asdfasdfasdf

--- haiku_baselines/PPO/ppo.py
+++ haiku_baselines/PPO/ppo.py
@@ -111,8 +111,7 @@ class PPO(Actor_Critic_Policy_Gradient_Family):
         
         batched_obses = list(zip(*[jnp.split(o, self.minibatch_size) for o in obses]))
         for bb in batched_obses:
-            for b in bb:
-                print(b.shape)
+            print(bb.shape)
         batched_actions = jnp.split(actions, self.minibatch_size)
         batched_targets = jnp.split(targets, self.minibatch_size)
         batched_value = jnp.split(value, self.minibatch_size)
