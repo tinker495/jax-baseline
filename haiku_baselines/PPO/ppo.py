2e1018d3d2250c5b233a4ad7be75ee02006dbcde
TinkTheBoush <wjdrbtjr495@gmail.com>
2022-01-17 16:24:52 +0900

    asdfasdfasdf

--- haiku_baselines/PPO/ppo.py
+++ haiku_baselines/PPO/ppo.py
@@ -112,13 +112,12 @@ class PPO(Actor_Critic_Policy_Gradient_Family):
             params = optax.apply_updates(params, updates)
             return (params, opt_state), (c_loss, a_loss)
         
-        batched_obses =  [jnp.split(o[idxes], batch_n) for o in obses]
+        batched_obses =  [list(zo)for zo in zip(*[jnp.split(o[idxes], batch_n) for o in obses])]
         batched_actions = jnp.split(actions[idxes], batch_n)
         batched_targets = jnp.split(targets[idxes], batch_n)
         batched_value = jnp.split(value[idxes], batch_n)
         batched_act_prob = jnp.split(act_prob[idxes], batch_n)
         batched_adv = jnp.split(adv[idxes], batch_n)
-        print(len(batched_adv))
         (params, opt_state), (critic_loss, actor_loss) = \
                         jax.lax.scan(f,(params, opt_state),(batched_obses, batched_actions, batched_targets, batched_value, batched_act_prob, batched_adv))
 
