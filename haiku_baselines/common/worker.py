f1ac141bead49ba7d72c8eb712afa941cba04024
TinkTheBoush <wjdrbtjr495@gmail.com>
2022-01-19 11:17:17 +0900

    asdfasdfasdf

--- haiku_baselines/common/worker.py
+++ haiku_baselines/common/worker.py
@@ -66,7 +66,6 @@ class gymRayworker:
     def get_reset(self):
         return self.env.reset(),None,0,False,False
     
-    @property
     def get_info(self):
         return {'observation_space' : self.env.observation_space, 
                 'action_space' : self.env.action_space,
