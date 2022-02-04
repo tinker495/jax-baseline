7cb2ad3f94e293c9d9ab8c8a2b52e2c1f6380160
TinkTheBoush <wjdrbtjr495@gmail.com>
2022-02-04 15:17:35 +0900

    asdfasdfasdfasdf

--- haiku_baselines/common/Module.py
+++ haiku_baselines/common/Module.py
@@ -59,7 +59,7 @@ def visual_embedding(mode="simple"):
     elif mode == 'slide':
         net_fn = lambda x: hk.Sequential([
                     hk.Conv2D(64, kernel_shape=[3, 3], stride=[1, 1], padding='SAME'), jax.nn.relu,
-                    hk.Conv2D(128, kernel_shape=[3, 3], stride=[1, 1], padding='VALID'), jax.nn.relu,
+                    hk.Conv2D(128, kernel_shape=[1, 1], stride=[1, 1], padding='VALID'), jax.nn.relu,
                     hk.Flatten()
                     ])(x)
     elif mode == "none":
