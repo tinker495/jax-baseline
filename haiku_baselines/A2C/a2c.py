37fc37e559bd6f97e84188af31a87cba3463af53
TinkTheBoush <wjdrbtjr495@gmail.com>
2022-02-17 13:45:23 +0900

    asdfasdfasdf

--- haiku_baselines/A2C/a2c.py
+++ haiku_baselines/A2C/a2c.py
@@ -83,12 +83,12 @@ class A2C(Actor_Critic_Policy_Gradient_Family):
                                     )
     
     def action_discrete(self,obs,steps):
-        prob = self._get_actions(self.params, obs)
+        prob = np.array(self._get_actions(self.params, obs))
         return np.expand_dims(np.stack([np.random.choice(self.action_size[0],p=p) for p in prob],axis=0),axis=1)
     
     def action_continuous(self,obs,steps):
         mu, std = self._get_actions(self.params, obs)
-        return np.random.normal(mu, std)
+        return np.random.normal(np.array(mu), np.array(std))
     
     def train_step(self, steps):
         # Sample a batch from the replay buffer
