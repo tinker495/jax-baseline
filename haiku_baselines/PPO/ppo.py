03ed8756bee118eae6bfc81c8a672b488b9fe60f
ksjung <TinkTheBoush@github.com>
2022-02-19 17:01:17 +0900

    asdfasdfasdfasdf

--- haiku_baselines/PPO/ppo.py
+++ haiku_baselines/PPO/ppo.py
@@ -92,7 +92,7 @@ class PPO(Actor_Critic_Policy_Gradient_Family):
                                     )
     
     def action_discrete(self,obs,steps):
-        prob = self._get_actions(self.params, obs)
+        prob = np.array(self._get_actions(self.params, obs))
         return np.expand_dims(np.stack([np.random.choice(self.action_size[0],p=p) for p in prob],axis=0),axis=1)
     
     def action_continuous(self,obs,steps):
