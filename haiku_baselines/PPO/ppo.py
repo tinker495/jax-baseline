a08d323ac01e7867087856156612c5a5217a07af
ksjung <TinkTheBoush@github.com>
2022-02-20 15:48:36 +0900

    asdfasdfasdfasdf

--- haiku_baselines/PPO/ppo.py
+++ haiku_baselines/PPO/ppo.py
@@ -79,13 +79,13 @@ class PPO(Actor_Critic_Policy_Gradient_Family):
         mu, log_std = prob
         std = jnp.exp(log_std)
         if out_prob:
-            return prob, - (0.5 * jnp.sum(jnp.square((action - mu) / (std + 1e-6)),axis=-1,keepdims=True) + 
+            return prob, - (0.5 * jnp.sum(jnp.square((action - mu) / (std + 1e-7)),axis=-1,keepdims=True) + 
                                    jnp.sum(log_std,axis=-1,keepdims=True) + 
-                                   0.5 * jnp.log(2 * np.pi)* jnp.asarray(action.shape[-1],dtype=jnp.float32))
+                                   0.5 * jnp.log(2 * np.pi))
         else:
-            return - (0.5 * jnp.sum(jnp.square((action - mu) / (std + 1e-6)),axis=-1,keepdims=True) + 
+            return - (0.5 * jnp.sum(jnp.square((action - mu) / (std + 1e-7)),axis=-1,keepdims=True) + 
                              jnp.sum(log_std,axis=-1,keepdims=True) + 
-                             0.5 * jnp.log(2 * np.pi)* jnp.asarray(action.shape[-1],dtype=jnp.float32))
+                             0.5 * jnp.log(2 * np.pi))
     
     
     def discription(self):
