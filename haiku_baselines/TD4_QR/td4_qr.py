482d3ccd458dfbf70c11e5aa3e5940cd251a7a38
ksjung <TinkTheBoush@github.com>
2022-02-25 11:23:04 +0900

    asdfasdfasdfasdf

--- haiku_baselines/TD4_QR/td4_qr.py
+++ haiku_baselines/TD4_QR/td4_qr.py
@@ -136,8 +136,8 @@ class TD4_QR(Deteministic_Policy_Gradient_Family):
                       + jnp.clip(self.traget_action_noise*jax.random.normal(key,(self.batch_size,self.action_size[0])),-self.action_noise_clamp,self.action_noise_clamp)
                       ,-1.0,1.0)
         q1, q2 = self.critic.apply(target_params, key, next_feature, next_action)
-        next_q = truncated_mixture((q1, q2),self.n_support*2 - 2)
-        #next_q = jnp.minimum(q1,q2)
+        #next_q = truncated_mixture((q1, q2),self.n_support*2 - 2)
+        next_q = jnp.minimum(q1,q2)
         return (not_dones * next_q * self._gamma) + rewards
     
     def learn(self, total_timesteps, callback=None, log_interval=100, tb_log_name="TD4_QR",
