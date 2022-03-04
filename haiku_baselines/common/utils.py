60be7a11db860b522c3c0b693c7be41e487b7a37
ksjung <TinkTheBoush@github.com>
2022-03-04 10:08:43 +0900

    asdfasdfasdf

--- haiku_baselines/common/utils.py
+++ haiku_baselines/common/utils.py
@@ -82,15 +82,14 @@ def get_gaes(rewards, dones, terminals, values, next_values, gamma, lamda):
 
 def formatData(t,s):
   if not isinstance(t,dict) and not isinstance(t,list):
-      print(": "+str(t), end ="")
+    print(": "+str(t), end ="")
   else:
-      for key in t:
-          print("\n\t"*s+str(key), end ="")
-          if not isinstance(t,list):
-              formatData(t[key],s+1)
-      print()
+    for key in t:
+      print("\n"+"\t"*s+str(key), end ="")
+      if not isinstance(t,list):
+        formatData(t[key],s+1)
 
 def print_param(name,params):
-  print(name)
+  print(name, end ="")
   param_tree_map = jax.tree_map(lambda x: x.shape, params)
   formatData(param_tree_map,1)
