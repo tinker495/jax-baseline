ed73386f750fffa752a779bc641356790df1d397
ksjung <TinkTheBoush@github.com>
2022-03-14 09:24:55 +0900

    ASDFASDFASDFasdf

--- haiku_baselines/common/atari_wrappers.py
+++ haiku_baselines/common/atari_wrappers.py
@@ -290,7 +290,9 @@ def wrap_deepmind(env, episode_life=True, clip_rewards=True, frame_stack=False, 
 def make_wrap_atari(env_id='Breakout-v0', clip_rewards=True):
 	#env = gym.make(env_id)
 	env = make_atari(env_id)
-	return wrap_deepmind(env, clip_rewards=clip_rewards, frame_stack=True)
+	env = wrap_deepmind(env, clip_rewards=clip_rewards, frame_stack=True)
+	env = TimeLimit(env, max_episode_steps=10000)
+	return env
 
 def get_env_type(env_id):
     _game_envs = defaultdict(set)
