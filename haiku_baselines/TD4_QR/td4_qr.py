e8df886d3bc92456a41ca597edbb08bfc3b8e6dd
ksjung <TinkTheBoush@github.com>
2022-03-15 20:34:49 +0900

    asdfasdfasdfdasdf

--- haiku_baselines/TD4_QR/td4_qr.py
+++ haiku_baselines/TD4_QR/td4_qr.py
@@ -152,6 +152,7 @@ class TD4_QR(Deteministic_Policy_Gradient_Family):
     
     def learn(self, total_timesteps, callback=None, log_interval=100, tb_log_name="TD4_QR",
               reset_num_timesteps=True, replay_wrapper=None):
+        tb_log_name = tb_log_name + "({:d})".format(self.n_support)
         if self.risk_avoidance != 0.0:
-            tb_log_name = tb_log_name + "({:d})_riskavoid{:.2f}".format(self.n_support,self.risk_avoidance)
+            tb_log_name = tb_log_name + "_riskavoid{:.2f}".format(self.risk_avoidance)
         super().learn(total_timesteps, callback, log_interval, tb_log_name, reset_num_timesteps, replay_wrapper)
