5b18b9d325bc3f3c7bd35994c8e93bf0f99c8cc6
ksjung <TinkTheBoush@github.com>
2022-07-11 15:09:32 +0900

    asdfasdf

--- test/test_dqn_breakout.sh
+++ test/test_dqn_breakout.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES=0
 export SDL_VIDEODRIVER=dummy
-python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0001 --steps 1e7 --batch 32 --train_freq 4 --target_update 1000 --node 512 --hidden_n 1 --final_eps 0.05 --learning_starts 20000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.3
+#python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0001 --steps 1e7 --batch 32 --train_freq 4 --target_update 1000 --node 512 --hidden_n 1 --final_eps 0.05 --learning_starts 20000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.3
 python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0001 --steps 1e7 --batch 32 --train_freq 4 --target_update 1000 --node 512 --hidden_n 1 --final_eps 0.05 --learning_starts 20000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.3 --per
-python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0001 --steps 1e7 --batch 32 --train_freq 4 --target_update 1000 --node 512 --hidden_n 1 --final_eps 0.05 --learning_starts 20000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.3 --n_step 3
-python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0001 --steps 1e7 --batch 32 --train_freq 4 --target_update 1000 --node 512 --hidden_n 1 --final_eps 0.05 --learning_starts 20000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.3 --per --n_step 3
+#python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0001 --steps 1e7 --batch 32 --train_freq 4 --target_update 1000 --node 512 --hidden_n 1 --final_eps 0.05 --learning_starts 20000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.3 --n_step 3
+#python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0001 --steps 1e7 --batch 32 --train_freq 4 --target_update 1000 --node 512 --hidden_n 1 --final_eps 0.05 --learning_starts 20000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.3 --per --n_step 3
