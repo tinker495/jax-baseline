89df8ead8f3c9c1dfb839a26deeedb358842704a
ksjung <TinkTheBoush@github.com>
2022-07-11 15:08:33 +0900

    asdfasdfasdfasdfasdf

--- haiku_baselines/DQN/dqn.py
+++ haiku_baselines/DQN/dqn.py
@@ -101,7 +101,7 @@ class DQN(Q_Network_Family):
     def _loss(self, params, obses, actions, targets, weights, key):
         vals = jnp.take_along_axis(self.get_q(params, obses, key), actions, axis=1)
         error = jnp.squeeze(vals - targets)
-        return jnp.mean(jnp.squeeze(weights)*jnp.square(error)), jnp.abs(error)
+        return jnp.mean(jnp.square(error)*jnp.squeeze(weights)), jnp.abs(error)
     
     def _target(self,params, target_params, obses, actions, rewards, nxtobses, not_dones, key):
         next_q = self.get_q(target_params,nxtobses,key)
