45761b0e201cb8bcdedcd559ff09410d963f522e
ksjung <TinkTheBoush@github.com>
2022-07-12 17:54:13 +0900

    asdfasdfasdfasdfasdfasdf

--- haiku_baselines/common/utils.py
+++ haiku_baselines/common/utils.py
@@ -42,7 +42,7 @@ def q_log_pi(q,entropy_tau):
   q_submax = q - jnp.max(q, axis=1, keepdims=True)
   logsum = jax.nn.logsumexp(q_submax/entropy_tau,axis=1,keepdims=True)
   tau_log_pi = (q_submax - entropy_tau*logsum)
-  pi = jax.nn.softmax((q - jnp.min(q, axis=1, keepdims=True))/entropy_tau)
+  pi = jax.nn.softmax(q/entropy_tau)
   return q_submax, tau_log_pi, pi
 
 @jax.jit
