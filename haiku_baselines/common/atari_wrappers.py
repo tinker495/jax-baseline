65a8d948c1899155a3fe7da61d9d99a68b645e47
TinkTheBoush <wjdrbtjr495@gmail.com>
2023-01-27 17:01:21 +0900

    asdfasdfasdfasdf

--- haiku_baselines/common/atari_wrappers.py
+++ haiku_baselines/common/atari_wrappers.py
@@ -102,7 +102,7 @@ class EpisodicLifeEnv(gym.Wrapper):
             obs, info = self.env.reset(**kwargs)
         else:
             # no-op step to advance from terminal/lost life state
-            obs, _, _, _, _ = self.env.step(0)
+            obs, _, _, _, info = self.env.step(0)
         self.lives = self.env.unwrapped.ale.lives()
         return obs, info
 
