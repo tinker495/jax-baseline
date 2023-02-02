277165cfeb67013b30bb49b5da3fc6582cf00336
TinkTheBoush <TinkTheBoush@github.com>
2023-02-03 01:19:01 +0900

    asdfasdfasdf

--- test/test_dqn_breakout.sh
+++ test/test_dqn_breakout.sh
@@ -1,13 +1,8 @@
 pip install .. -q
 export SDL_VIDEODRIVER=dummy
 export CUDA_VISIBLE_DEVICES=0
-#python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0002 --steps 1e7 --batch 32 --train_freq 1 --target_update 10000 --node 512 --hidden_n 1 --final_eps 0.05 --learning_starts 20000 --gamma 0.99 --buffer_size 2e5 --munchausen&
-export CUDA_VISIBLE_DEVICES=1
-python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0002 --steps 1e7 --batch 32 --train_freq 4 --target_update 10000 --node 512 --hidden_n 1 --final_eps 0.01 --learning_starts 20000 --gamma 0.995 --buffer_size 2e5
-#python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0001 --steps 1e7 --batch 32 --train_freq 4 --target_update 10000 --node 512 --hidden_n 1 --final_eps 0.05 --learning_starts 20000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.3 --n_step 3
-#python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0001 --steps 1e7 --batch 32 --train_freq 4 --target_update 10000 --node 512 --hidden_n 1 --final_eps 0.05 --learning_starts 20000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.3 --per --n_step 3
-#zip log.zip -r log/
+python3 run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0002 --steps 1e6 --batch 32 --train_freq 1 --target_update 5000 --node 256 --hidden_n 1 --final_eps 0.05 --exploration_fraction 0.8 --learning_starts 10000 --gamma 0.995 --buffer_size 1e6 --clip_rewards
 
-#python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0002 --steps 1e7 --batch 32 --train_freq 4 --target_update 10000 --node 512 --hidden_n 1 --final_eps 0.01 --learning_starts 20000 --gamma 0.995 --buffer_size 2e5 --clip_rewards --double --dueling --per --munchausen
+python3 run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0002 --steps 1e6 --batch 32 --train_freq 1 --target_update 5000 --node 256 --hidden_n 1 --final_eps 0.05 --exploration_fraction 0.8 --learning_starts 10000 --gamma 0.995 --buffer_size 1e6 --clip_rewards --per
 
-#python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0002 --steps 1e7 --batch 32 --train_freq 4 --target_update 10000 --node 512 --hidden_n 1 --final_eps 0.01 --learning_starts 20000 --gamma 0.995 --buffer_size 2e5 --double --dueling --per --n_step 3 --noisynet
+python3 run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0002 --steps 1e6 --batch 32 --train_freq 1 --target_update 5000 --node 256 --hidden_n 1 --final_eps 0.05 --exploration_fraction 0.8 --learning_starts 10000 --gamma 0.995 --buffer_size 1e6 --clip_rewards --n_step 4
