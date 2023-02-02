68b858de5e479ecb023a85c2a0e264326e41212e
TinkTheBoush <TinkTheBoush@github.com>
2023-02-02 23:28:16 +0900

    asdfasdf

--- haiku_baselines/common/atari_wrappers.py
+++ haiku_baselines/common/atari_wrappers.py
@@ -50,7 +50,7 @@ class FireResetEnv(gym.Wrapper):
         gym.Wrapper.__init__(self, env)
         assert env.unwrapped.get_action_meanings()[1] == 'FIRE'
         assert len(env.unwrapped.get_action_meanings()) >= 3
-        self._action_space = gym.spaces.Discrete(self.action_space-2)
+        self._action_space = gym.spaces.Discrete(self.action_space.n-2)
 
     def reset(self, **kwargs):
         self.env.reset(**kwargs)
