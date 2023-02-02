8f65984923d288e99abdb3468fc530c45e8e3bd4
TinkTheBoush <TinkTheBoush@github.com>
2023-02-02 23:33:44 +0900

    asdfasdfasdfasdfasdf

--- haiku_baselines/common/atari_wrappers.py
+++ haiku_baselines/common/atari_wrappers.py
@@ -50,7 +50,7 @@ class FireResetEnv(gym.Wrapper):
         gym.Wrapper.__init__(self, env)
         assert env.unwrapped.get_action_meanings()[1] == 'FIRE'
         assert len(env.unwrapped.get_action_meanings()) >= 3
-        self._action_space = gym.spaces.Discrete(self.action_space.n-2)
+        #self._action_space = gym.spaces.Discrete(self.action_space.n-2)
 
     def reset(self, **kwargs):
         self.env.reset(**kwargs)
@@ -63,7 +63,7 @@ class FireResetEnv(gym.Wrapper):
         return obs, info
 
     def step(self, action):
-        return self.env.step(action+2)
+        return self.env.step(action)
 
 class EpisodicLifeEnv(gym.Wrapper):
     def __init__(self, env):
