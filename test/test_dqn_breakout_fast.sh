4fa212913fdedb6a5c85870327bd7b640da48cf9
TinkTheBoush <wjdrbtjr495@gmail.com>
2023-02-14 15:34:13 +0900

    asdfasdf

--- test/test_dqn_breakout_fast.sh
+++ test/test_dqn_breakout_fast.sh
@@ -1,17 +1,19 @@
 export CUDA_VISIBLE_DEVICES=0
 export SDL_VIDEODRIVER=dummy
-OPTIMIZER="rmsprop"
-python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0003 --steps 1e7 --batch 16 --train_freq 1 --target_update 500 --node 128 --hidden_n 1 --final_eps 0.05 --learning_starts 1000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.1 --clip_rewards --optimizer $OPTIMIZER
-python run_qnet.py --algo C51 --env BreakoutNoFrameskip-v4 --learning_rate 0.0003 --steps 1e7 --batch 16 --train_freq 1 --target_update 500 --node 128 --hidden_n 1 --final_eps 0.05 --learning_starts 1000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.1 --min 0 --max 30 --clip_rewards --optimizer $OPTIMIZER
-python run_qnet.py --algo QRDQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0003 --steps 1e7 --batch 16 --train_freq 1 --target_update 500 --node 128 --hidden_n 1 --final_eps 0.05 --learning_starts 1000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.1 --n_support 200 --delta 0.1 --clip_rewards --optimizer $OPTIMIZER
-python run_qnet.py --algo IQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0003 --steps 1e7 --batch 16 --train_freq 1 --target_update 500 --node 128 --hidden_n 1 --final_eps 0.05 --learning_starts 1000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.1 --n_support 64 --delta 0.1 --clip_rewards --optimizer $OPTIMIZER
+ENV="--env BreakoutNoFrameskip-v4 --clip_rewards"
+TRAIN="--learning_rate 0.0003 --steps 3e6 --batch 16 --train_freq 1 --target_update 500 --final_eps 0.05 --learning_starts 1000 --gamma 0.995 --buffer_size 1e6 --exploration_fraction 0.3"
+OPTIMIZER="--optimizer rmsprop"
+python run_qnet.py --algo DQN $ENV $TRAIN $OPTIMIZER
+python run_qnet.py --algo C51 $ENV $TRAIN $OPTIMIZER --min 0 --max 30
+python run_qnet.py --algo QRDQN $ENV $TRAIN $OPTIMIZER --n_support 200 --delta 0.1
+python run_qnet.py --algo IQN $ENV $TRAIN $OPTIMIZER --n_support 64 --delta 0.1
 OPTIMIZER="adam"
-python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0003 --steps 1e7 --batch 16 --train_freq 1 --target_update 500 --node 128 --hidden_n 1 --final_eps 0.05 --learning_starts 1000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.1 --clip_rewards --optimizer $OPTIMIZER
-python run_qnet.py --algo C51 --env BreakoutNoFrameskip-v4 --learning_rate 0.0003 --steps 1e7 --batch 16 --train_freq 1 --target_update 500 --node 128 --hidden_n 1 --final_eps 0.05 --learning_starts 1000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.1 --min 0 --max 30 --clip_rewards --optimizer $OPTIMIZER
-python run_qnet.py --algo QRDQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0003 --steps 1e7 --batch 16 --train_freq 1 --target_update 500 --node 128 --hidden_n 1 --final_eps 0.05 --learning_starts 1000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.1 --n_support 200 --delta 0.1 --clip_rewards --optimizer $OPTIMIZER
-python run_qnet.py --algo IQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0003 --steps 1e7 --batch 16 --train_freq 1 --target_update 500 --node 128 --hidden_n 1 --final_eps 0.05 --learning_starts 1000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.1 --n_support 64 --delta 0.1 --clip_rewards --optimizer $OPTIMIZER
+python run_qnet.py --algo DQN $ENV $TRAIN $OPTIMIZER
+python run_qnet.py --algo C51 $ENV $TRAIN $OPTIMIZER --min 0 --max 30
+python run_qnet.py --algo QRDQN $ENV $TRAIN $OPTIMIZER --n_support 200 --delta 0.1
+python run_qnet.py --algo IQN $ENV $TRAIN $OPTIMIZER --n_support 64 --delta 0.1
 OPTIMIZER="adamw"
-python run_qnet.py --algo DQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0003 --steps 1e7 --batch 16 --train_freq 1 --target_update 500 --node 128 --hidden_n 1 --final_eps 0.05 --learning_starts 1000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.1 --clip_rewards --optimizer $OPTIMIZER
-python run_qnet.py --algo C51 --env BreakoutNoFrameskip-v4 --learning_rate 0.0003 --steps 1e7 --batch 16 --train_freq 1 --target_update 500 --node 128 --hidden_n 1 --final_eps 0.05 --learning_starts 1000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.1 --min 0 --max 30 --clip_rewards --optimizer $OPTIMIZER
-python run_qnet.py --algo QRDQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0003 --steps 1e7 --batch 16 --train_freq 1 --target_update 500 --node 128 --hidden_n 1 --final_eps 0.05 --learning_starts 1000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.1 --n_support 200 --delta 0.1 --clip_rewards --optimizer $OPTIMIZER
-python run_qnet.py --algo IQN --env BreakoutNoFrameskip-v4 --learning_rate 0.0003 --steps 1e7 --batch 16 --train_freq 1 --target_update 500 --node 128 --hidden_n 1 --final_eps 0.05 --learning_starts 1000 --gamma 0.99 --buffer_size 5e5 --exploration_fraction 0.1 --n_support 64 --delta 0.1 --clip_rewards --optimizer $OPTIMIZER
+python run_qnet.py --algo DQN $ENV $TRAIN $OPTIMIZER
+python run_qnet.py --algo C51 $ENV $TRAIN $OPTIMIZER --min 0 --max 30
+python run_qnet.py --algo QRDQN $ENV $TRAIN $OPTIMIZER --n_support 200 --delta 0.1
+python run_qnet.py --algo IQN $ENV $TRAIN $OPTIMIZER --n_support 64 --delta 0.1
