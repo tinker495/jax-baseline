7e3b8887bb4f891a4d8ae0e3f8d32bdb9a6c2a12
TinkTheBoush <TinkTheBoush@github.com>
2023-02-16 00:02:47 +0900

    asdfasdfasdf

--- test/test_dqn_breakout_fast.sh
+++ test/test_dqn_breakout_fast.sh
@@ -2,7 +2,7 @@ export CUDA_VISIBLE_DEVICES=0
 export SDL_VIDEODRIVER=dummy
 ENV="--env LunarLander-v2"
 TRAIN="--learning_rate 0.0002 --steps 5e5 --batch 32 --train_freq 1 --target_update 1000 --final_eps 0.1 --learning_starts 1000 --gamma 0.99 --buffer_size 1e5 --exploration_fraction 0.6"
-MODEL="--node 128 --hidden_n 2"
+MODEL="--node 512 --hidden_n 2"
 OPTIONS=""
 OPTIMIZER="--optimizer rmsprop"
 python run_qnet.py --algo DQN $ENV $TRAIN $MODEL $OPTIMIZER $OPTIONS
