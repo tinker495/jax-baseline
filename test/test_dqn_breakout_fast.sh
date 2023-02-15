760cd824ced96679aff56b27c4f9e586a3674107
TinkTheBoush <wjdrbtjr495@gmail.com>
2023-02-15 16:36:07 +0900

    asdfasdf

--- test/test_dqn_breakout_fast.sh
+++ test/test_dqn_breakout_fast.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0
 export SDL_VIDEODRIVER=dummy
 ENV="--env BreakoutNoFrameskip-v4 --clip_rewards"
-TRAIN="--learning_rate 0.0001 --steps 1e7 --batch 32 --train_freq 4 --target_update 1000 --final_eps 0.01 --learning_starts 1000 --gamma 0.99 --buffer_size 1e5 --exploration_fraction 0.1"
+TRAIN="--learning_rate 0.0001 --steps 1e7 --batch 32 --train_freq 1 --target_update 1000 --final_eps 0.01 --learning_starts 100000 --gamma 0.99 --buffer_size 1e5 --exploration_fraction 0.3"
 MODEL="--node 512 --hidden_n 0"
 OPTIONS=""
 OPTIMIZER="--optimizer rmsprop"
