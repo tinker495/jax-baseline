48b36ee5fa310d5bc38b2e29eb5ceb025de8572f
TinkTheBoush <wjdrbtjr495@gmail.com>
2023-02-22 19:08:35 +0900

    asdfasdf

--- test/test_dqn_breakout_fast.sh
+++ test/test_dqn_breakout_fast.sh
@@ -1,11 +1,11 @@
 export CUDA_VISIBLE_DEVICES=0
 export SDL_VIDEODRIVER=dummy
 ENV="--env BreakoutNoFrameskip-v4"
-RL="--learning_rate 0.0002"
+RL="--learning_rate 0.00005"
 TRAIN="--steps 5e5 --batch 32 --train_freq 1 --target_update 1000 --final_eps 0.01 --learning_starts 1000 --gamma 0.995 --buffer_size 1e5 --exploration_fraction 0.3"
 MODEL="--node 512 --hidden_n 0"
 OPTIONS=""
-OPTIMIZER="--optimizer adamw"
+OPTIMIZER="--optimizer lion"
 python run_qnet.py --algo DQN $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS
 python run_qnet.py --algo C51 $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS --min 0 --max 30
 python run_qnet.py --algo QRDQN $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS --n_support 200 --delta 0.1
