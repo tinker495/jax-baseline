3bc2535d45d7edc15be2abfb3437df162869a0ef
TinkTheBoush <wjdrbtjr495@gmail.com>
2023-02-22 19:17:33 +0900

    asdfasdf

--- test/test_dqn_breakout_fast.sh
+++ test/test_dqn_breakout_fast.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0
 export SDL_VIDEODRIVER=dummy
 ENV="--env BreakoutNoFrameskip-v4"
-RL="--learning_rate 0.00005"
+RL="--learning_rate 0.0001"
 TRAIN="--steps 5e5 --batch 32 --train_freq 1 --target_update 1000 --final_eps 0.01 --learning_starts 1000 --gamma 0.995 --buffer_size 1e5 --exploration_fraction 0.3"
 MODEL="--node 512 --hidden_n 0"
 OPTIONS=""
