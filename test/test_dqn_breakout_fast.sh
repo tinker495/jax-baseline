412a6bfba0e7bd7149fe5b13f7bbcca6d2c89588
TinkTheBoush <wjdrbtjr495@gmail.com>
2023-02-23 15:20:02 +0900

    asdfasdf

--- test/test_dqn_breakout_fast.sh
+++ test/test_dqn_breakout_fast.sh
@@ -4,7 +4,7 @@ ENV="--env BreakoutNoFrameskip-v4"
 RL="--learning_rate 0.0002"
 TRAIN="--steps 5e5 --batch 32 --train_freq 1 --target_update 1000 --final_eps 0.01 --learning_starts 1000 --gamma 0.995 --buffer_size 1e5 --exploration_fraction 0.3"
 MODEL="--node 512 --hidden_n 0"
-OPTIONS="--n_step 4"
+OPTIONS="--double --munchausen"
 OPTIMIZER="--optimizer adamw"
 python run_qnet.py --algo DQN $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS
 #python run_qnet.py --algo C51 $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS --min 0 --max 30
