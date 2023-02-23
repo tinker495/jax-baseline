b192cff064e3636adfd3c92f591be56a20ce82ce
TinkTheBoush <TinkTheBoush@github.com>
2023-02-24 00:51:11 +0900

    asdfasdfasdfasdf

--- test/test_dpg_walker.sh
+++ test/test_dpg_walker.sh
@@ -5,7 +5,7 @@ ENV="--env ../../env/Walker.x86_64"
 RL="--learning_rate 0.0002"
 TRAIN="--steps 2e6 --batch 256 --target_update_tau 5e-4 --learning_starts 1000"
 MODEL="--node 512 --hidden_n 3"
-OPTIONS=""
+OPTIONS="--time_scale 36"
 OPTIMIZER="--optimizer adam"
 python  run_dpg.py --algo TD3 $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS
 python  run_dpg.py --algo TD4_QR $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS
