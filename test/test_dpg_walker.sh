ee82acdeb1b39d03bdbe4f1fb61946d6146aa22f
TinkTheBoush <wjdrbtjr495@gmail.com>
2023-02-23 18:40:02 +0900

    asdfasdf

--- test/test_dpg_walker.sh
+++ test/test_dpg_walker.sh
@@ -3,8 +3,8 @@ export SDL_VIDEODRIVER=dummy
 
 ENV="--env ../../env/Walker.x86_64"
 RL="--learning_rate 0.0002"
-TRAIN="--steps 2e7 --batch 256 --target_update_tau 5e-4 --learning_starts 1000"
+TRAIN="--steps 2e6 --batch 256 --target_update_tau 5e-4 --learning_starts 1000"
 MODEL="--node 512 --hidden_n 3"
 OPTIONS=""
-OPTIMIZER="--optimizer adamw"
+OPTIMIZER="--optimizer lion"
 python  run_dpg.py --algo TQC $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS
