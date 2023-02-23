5a2185fd8be8300309ecca8dd833dd425e5507c5
TinkTheBoush <TinkTheBoush@github.com>
2023-02-23 22:29:53 +0900

    asdfasdfasdf

--- test/test_dpg_walker.sh
+++ test/test_dpg_walker.sh
@@ -6,5 +6,5 @@ RL="--learning_rate 0.0002"
 TRAIN="--steps 2e6 --batch 256 --target_update_tau 5e-4 --learning_starts 1000"
 MODEL="--node 512 --hidden_n 3"
 OPTIONS=""
-OPTIMIZER="--optimizer adamw"
+OPTIMIZER="--optimizer adam"
 python  run_dpg.py --algo TQC $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS
