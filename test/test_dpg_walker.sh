63ed193cabc9b3ac288d822f81e55f88ac355794
TinkTheBoush <TinkTheBoush@github.com>
2023-02-23 22:30:11 +0900

    asdfasdf

--- test/test_dpg_walker.sh
+++ test/test_dpg_walker.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0
 export SDL_VIDEODRIVER=dummy
 
-ENV="--env ../../env/Walker.x86_64"
+ENV="--env ../../env/Crawler.x86_64"
 RL="--learning_rate 0.0002"
 TRAIN="--steps 2e6 --batch 256 --target_update_tau 5e-4 --learning_starts 1000"
 MODEL="--node 512 --hidden_n 3"
