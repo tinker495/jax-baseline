3d83a5472d65b088de44a9e60ccbac935d6a658a
TinkTheBoush <wjdrbtjr495@gmail.com>
2023-03-27 16:41:30 +0900

    asdfasdf

--- test/test_dpg_walker.sh
+++ test/test_dpg_walker.sh
@@ -8,9 +8,9 @@ TRAIN="--steps 2e6 --buffer_size 1e6 --batch 64 --target_update_tau 5e-3 --learn
 MODEL="--node 512 --hidden_n 3"
 OPTIONS="--n_support 25 --time_scale 10 --mixture truncated --quantile_drop 0.05" 
 OPTIMIZER="--optimizer adam"
-python  run_dpg.py --algo TD3 $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS --worker_id 0 &
-python  run_dpg.py --algo SAC $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS --worker_id 1 &
-python  run_dpg.py --algo TD4_QR $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS --worker_id 2 &
+python  run_dpg.py --algo TD3 $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS
+python  run_dpg.py --algo SAC $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS
+python  run_dpg.py --algo TD4_QR $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS
 python  run_dpg.py --algo TD4_IQN $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS
-python  run_dpg.py --algo TQC $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS --worker_id 3
-python  run_dpg.py --algo TQC_IQN $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS --worker_id 3
+python  run_dpg.py --algo TQC $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS
+python  run_dpg.py --algo TQC_IQN $ENV $RL $TRAIN $MODEL $OPTIMIZER $OPTIONS
