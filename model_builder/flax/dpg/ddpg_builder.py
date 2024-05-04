c866c5ac02e67a95c6f9a316bad276b7df625bdf
tinker495 <wjdrbtjr495@gmail.com>
2024-05-04 12:06:51 +0900

    ddpg

--- model_builder/flax/dpg/ddpg_builder.py
+++ model_builder/flax/dpg/ddpg_builder.py
@@ -51,7 +51,7 @@ def model_builder_maker(observation_space, action_size, policy_kwargs):
         embedding_mode = "normal"
 
     def model_builder(key=None, print_model=False):
-        class Merged(nn.Module):
+        class Actor_Merged(nn.Module):
             def setup(self):
                 self.preproc = PreProcess(observation_space, embedding_mode=embedding_mode)
                 self.act = Actor(action_size, **policy_kwargs)
@@ -60,8 +60,7 @@ def model_builder_maker(observation_space, action_size, policy_kwargs):
             def __call__(self, x):
                 feature = self.preprocess(x)
                 action = self.actor(feature)
-                q = self.critic(feature, action)
-                return q
+                return action
 
             def preprocess(self, x):
                 x = self.preproc(x)
@@ -70,23 +69,27 @@ def model_builder_maker(observation_space, action_size, policy_kwargs):
             def actor(self, x):
                 return self.act(x)
 
-            def critic(self, x, a):
-                return self.crit(x, a)
-
-        model = Merged()
-        preproc_fn = get_apply_fn_flax_module(model, model.preprocess)
-        actor_fn = get_apply_fn_flax_module(model, model.actor)
-        critic_fn = get_apply_fn_flax_module(model, model.critic)
+        actor_model = Actor_Merged()
+        critic_model = Critic(**policy_kwargs)
+        preproc_fn = get_apply_fn_flax_module(actor_model, actor_model.preprocess)
+        actor_fn = get_apply_fn_flax_module(actor_model, actor_model.actor)
+        critic_fn = get_apply_fn_flax_module(critic_model)
         if key is not None:
-            params = model.init(
+            policy_params = actor_model.init(
                 key,
                 [np.zeros((1, *o), dtype=np.float32) for o in observation_space],
             )
+            critic_params = critic_model.init(
+                key,
+                preproc_fn(policy_params, key, [np.zeros((1, *o), dtype=np.float32) for o in observation_space]),
+                np.zeros((1, *action_size), dtype=np.float32),
+            )
             if print_model:
                 print("------------------build-flax-model--------------------")
-                print_param("", params)
+                print_param("", policy_params)
+                print_param("", critic_params)
                 print("------------------------------------------------------")
-            return preproc_fn, actor_fn, critic_fn, params
+            return preproc_fn, actor_fn, critic_fn, policy_params, critic_params
         else:
             return preproc_fn, actor_fn, critic_fn
 
