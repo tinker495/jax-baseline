6c65a08182d9dbbf50ba730204caf79bc4492910
tinker495 <wjdrbtjr495@gmail.com>
2024-06-29 15:53:53 +0900

    less replay

--- test/test_dqn_breakout.sh
+++ test/test_dqn_breakout.sh
@@ -5,7 +5,7 @@ pip install -q ..
 
 ENV="--env BreakoutNoFrameskip-v4"
 LR="--learning_rate 0.0001"
-TRAIN="--steps 5e7 --batch 32 --train_freq 4 --final_eps 0.01 --learning_starts 20000 --gamma 0.99 --buffer_size 1e6 --exploration_fraction 0.1"
+TRAIN="--steps 5e7 --batch 32 --train_freq 4 --final_eps 0.01 --learning_starts 20000 --gamma 0.99 --buffer_size 1e5 --exploration_fraction 0.1"
 MODEL="--node 512 --hidden_n 1"
 OPTIONS="--target_update 1000"
 OPTIMIZER="--optimizer adam"
