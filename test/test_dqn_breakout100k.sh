a483d5c5d2275af6c0a84ece0e3acf0af549fdfc
tinker495 <wjdrbtjr495@gmail.com>
2024-07-27 16:38:53 +0900

    asdf

--- test/test_dqn_breakout100k.sh
+++ test/test_dqn_breakout100k.sh
@@ -9,13 +9,13 @@ OPTIMIZER="--optimizer adam"
 MODEL="--node 512 --hidden_n 1"
 
 TRAIN="--steps 1e5 --batch 32 --train_freq 4 --final_eps 0.01 --learning_starts 2000 --gamma 0.99 --buffer_size 1e5 --exploration_fraction 0.2"
-OPTIONS="--target_update 1000 --gradient_steps 1 --double --dueling --per --n_step 10 --noisynet"
+OPTIONS="--target_update 1000 --gradient_steps 1 --double --dueling --per --n_step 3 --noisynet"
 python run_qnet.py --algo DQN $ENV $LR $TRAIN $MODEL $OPTIMIZER $OPTIONS
 python run_qnet.py --algo C51 $ENV $LR $TRAIN $MODEL $OPTIMIZER $OPTIONS
 
 # High replay ratio
 TRAIN="--steps 1e5 --batch 32 --train_freq 1 --final_eps 0.01 --learning_starts 2000 --gamma 0.99 --buffer_size 1e5 --exploration_fraction 0.2"
-OPTIONS="--target_update 1000 --gradient_steps 2 --double --dueling --per --n_step 10 --noisynet"
+OPTIONS="--target_update 1000 --gradient_steps 2 --double --dueling --per --n_step 3 --noisynet"
 python run_qnet.py --algo DQN $ENV $LR $TRAIN $MODEL $OPTIMIZER $OPTIONS
 python run_qnet.py --algo C51 $ENV $LR $TRAIN $MODEL $OPTIMIZER $OPTIONS
 OPTIONS="--gradient_steps 2"
