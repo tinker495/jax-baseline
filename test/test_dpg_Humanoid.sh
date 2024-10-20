50929b5695f835c85130b41bd43b8301cc0cfd8d
tinker495 <wjdrbtjr495@gmail.com>
2024-10-21 03:29:27 +0900

    fix

--- test/test_dpg_Humanoid.sh
+++ test/test_dpg_Humanoid.sh
@@ -14,6 +14,7 @@ OPTIMIZER="--optimizer adam"
 python  run_dpg.py --algo DAC $EXPERIMENT $ENV $LR $TRAIN $MODEL $OPTIMIZER $OPTIONS
 python  run_dpg.py --algo DAC $EXPERIMENT $ENV $LR $TRAIN $MODEL $OPTIMIZER $OPTIONS --simba
 python  run_dpg.py --algo SAC $EXPERIMENT $ENV $LR $TRAIN $MODEL $OPTIMIZER $OPTIONS
+python  run_dpg.py --algo TQC $EXPERIMENT $ENV $LR $TRAIN $MODEL $OPTIMIZER $OPTIONS
 #python  run_dpg.py --algo TD3 $EXPERIMENT $ENV $LR $TRAIN $MODEL $OPTIMIZER $OPTIONS --simba
 #python  run_dpg.py --algo TD3 $EXPERIMENT $ENV $LR $TRAIN $MODEL $OPTIMIZER $OPTIONS
 #python  run_dpg.py --algo SAC $EXPERIMENT $ENV $LR $TRAIN $MODEL $OPTIMIZER $OPTIONS --simba
