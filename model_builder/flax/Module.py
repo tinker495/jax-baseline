e720e947b365f190e21fbe6c72d9e9329a8a4272
tinker495 <wjdrbtjr495@gmail.com>
2025-05-15 20:40:18 +0900

    update batchrenorm

--- model_builder/flax/Module.py
+++ model_builder/flax/Module.py
@@ -234,6 +234,8 @@ class BatchReNorm(Module):
     scale_init: Callable[[PRNGKey, Shape, Dtype], Array] = initializers.ones
     axis_name: Optional[str] = None
     axis_index_groups: Any = None
+    r_max: float = 3.0
+    d_max: float = 5.0
     use_fast_variance: bool = True
 
     @compact
@@ -265,25 +267,6 @@ class BatchReNorm(Module):
             "batch_stats", "var", lambda s: jnp.ones(s, jnp.float32), feature_shape
         )
 
-        r_max = self.variable(
-            "batch_stats",
-            "r_max",
-            lambda val: jnp.array(val, dtype=jnp.float32),
-            3,
-        )
-        d_max = self.variable(
-            "batch_stats",
-            "d_max",
-            lambda val: jnp.array(val, dtype=jnp.float32),
-            5,
-        )
-        steps = self.variable(
-            "batch_stats",
-            "steps",
-            lambda val: jnp.array(val, dtype=jnp.float32),  # Changed from int32 to float32
-            0,
-        )
-
         if use_running_average:
             mean, var = ra_mean.value, ra_var.value
             custom_mean = mean
@@ -301,25 +284,17 @@ class BatchReNorm(Module):
             custom_var = var
             if not self.is_initializing():
                 # The code below is implemented following the Batch Renormalization paper
-                r = 1
-                d = 0
                 std = jnp.sqrt(var + self.epsilon)
                 ra_std = jnp.sqrt(ra_var.value + self.epsilon)
-                r = lax.stop_gradient(std / ra_std)
-                r = jnp.clip(r, 1 / r_max.value, r_max.value)
-                d = lax.stop_gradient((mean - ra_mean.value) / ra_std)
-                d = jnp.clip(d, -d_max.value, d_max.value)
-                tmp_var = var / (r**2)
-                tmp_mean = mean - d * jnp.sqrt(custom_var) / r
-
-                # Warm up batch renorm for 100_000 steps to build up proper running statistics
-                warmed_up = jnp.greater_equal(steps.value, 100_000).astype(jnp.float32)
-                custom_var = warmed_up * tmp_var + (1.0 - warmed_up) * custom_var
-                custom_mean = warmed_up * tmp_mean + (1.0 - warmed_up) * custom_mean
+                r = jnp.clip(std / ra_std, 1 / self.r_max, self.r_max)
+                r = lax.stop_gradient(r)
+                d = jnp.clip((mean - ra_mean.value) / ra_std, -self.d_max, self.d_max)
+                d = lax.stop_gradient(d)
+                custom_mean = mean - (std * d / r)
+                custom_var = (var + self.epsilon) / (r * r) - self.epsilon
 
                 ra_mean.value = self.momentum * ra_mean.value + (1 - self.momentum) * mean
                 ra_var.value = self.momentum * ra_var.value + (1 - self.momentum) * var
-                steps.value += 1
 
         return _normalize(
             self,
