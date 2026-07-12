cb77e5a55aac90bdf0460b27a0fde345a8123690
tinker495 <wjdrbtjr495@gmail.com>
2026-07-12 20:37:13 +0900

    refactor: use pprint for parameter trees

--- model_builder/utils.py
+++ model_builder/utils.py
@@ -1,19 +1,9 @@
+from pprint import pprint
+
 import jax
-
-
-def formatData(t, s):
-    if not isinstance(t, dict) and not isinstance(t, list):
-        print(": " + str(t), end="")
-    else:
-        for key in t:
-            print("\n" + "\t" * s + str(key), end="")
-            if not isinstance(t, list):
-                formatData(t[key], s + 1)
 
 
 def print_param(name, params):
     if name:
-        print(name, end="")
-    param_tree_map = jax.tree_util.tree_map(lambda x: x.shape, params)
-    formatData(param_tree_map, 1 if name else 0)
-    print()
+        print(name)
+    pprint(jax.tree_util.tree_map(lambda x: x.shape, params), sort_dicts=False)
